93541e9dcf037fa3e0b246a4c76362c7a32a6231
onesdh <onesdh@gmail.com>
2022-03-22 22:44:26 +0900

    3-4

---
+++ 이것이코딩테스트다/그리디/예제3-4.py
@@ -0,0 +1,21 @@
+# 3-4
+
+import sys
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+
+result = 0
+
+while N >= K :
+    while N % K != 0 :
+        N -= 1
+        result += 1
+    N = N / K
+    result += 1
+
+while N > 1 :
+    N -= 1
+    result += 1
+
+print(result)
