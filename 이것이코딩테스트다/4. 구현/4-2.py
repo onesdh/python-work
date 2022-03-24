ffebc905b6b53dae3a0da86a800004068bae16c7
onesdh <onesdh@gmail.com>
2022-03-24 21:58:25 +0900

    22-03-24

---
+++ 이것이코딩테스트다/4. 구현/4-2.py
@@ -0,0 +1,16 @@
+# 4-2
+
+import sys
+input = sys.stdin.readline
+
+N = int(input()) #(0 <= N <= 23)
+
+cnt = 0
+
+for i in range(N + 1) :
+    for j in range(60) :
+        for k in range(60) :
+            if '3' in str(i) + str(j) + str(k) :
+                cnt += 1
+
+print(cnt)
